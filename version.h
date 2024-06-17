78ffb3099942deac6739bdb15ae35f88389a9ec5
Jason Benaim <jkbenaim@gmail.com>
2024-06-17 18:43:52 -0400

    better help text

---
+++ version.h
@@ -0,0 +1,9 @@
+#pragma once
+#define PROG_NAME "pdscan"
+#define PROG_URL "https://jrra.zone/pdscan"
+#define PROG_VERSION_STRING "0.2"
+#define PROG_VERSION_MAJOR (0)
+#define PROG_VERSION_MINOR (2)
+#define PROG_VERSION (PROG_NAME "_" PROG_VERSION_STRING)
+#define PROG_EMBLEM (PROG_NAME "_" PROG_VERSION_STRING "\n" PROG_URL)
+#define PROG_COPYRIGHT "\xa9 2024 Jason 'jrra' Benaim, GPL v2"
